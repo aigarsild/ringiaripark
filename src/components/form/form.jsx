1995a896761086abb067df3d22e48b46d54b0e6c
Aigar Sild <aigar.sild@vaimo.com>
2020-05-22 15:17:44 +0300

    Added google recaptcha

--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
-import Button from 'react-bootstrap/Button'
+import Button from 'react-bootstrap/Button';
+import ReCAPTCHA from "react-google-recaptcha";
 
 class Form extends Component {
-   
+
   render() {
-    
+
     return (
       <>
         <form name="ringi-kontakt" method="POST" netlify-honeypot="bot-field" data-netlify="true" data-netlify-recaptcha="true" action="/thank-you">
-        <input type="hidden" name="form-name" value="ringi-kontakt" />
+          <input type="hidden" name="form-name" value="ringi-kontakt" />
           <p>
             <input type="text" required name="name" placeholder="Nimi*" />
           </p>
@@ -28,7 +29,9 @@ class Form extends Component {
               lisa isikuandmeid reklaami eesmärgiga andmebaasidesse.
               <u>Tutvu meie privaatsuspoliitikaga.</u>
             </p>
-            <p className="recaptcha" data-netlify-recaptcha="true"></p>
+            <p className="recaptcha">
+              <ReCAPTCHA sitekey="6LdqhPoUAAAAANKoaS2fpJcsg2L4kou4A2yBT2Uf" />
+            </p>
             <Button type="submit" className="font-weight-bold action" variant="danger">
               <span>Saada!</span>
             </Button>
